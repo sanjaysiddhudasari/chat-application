628fe0900cbd7937e69e31627218a1c04d6a11ad
sanjaysiddhu <sanjaysiddhu999@gmail.com>
2025-12-18 14:30:14 +0530

    initial backend

---
+++ index.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const socketio = require("socket.io");
+const http = require("http");
+const PORT = process.env.PORT || 5000;
+const router = require("./router");
+const { addUser, removeUser, getUser, getUsersInRoom } = require("./users");
+const { text } = require("stream/consumers");
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
+app.use(router);
+
+io.on("connection", (socket) => {
+  console.log("user is connected");
+  socket.on("join", ({ name, room }, callback) => {
+    const { error, user } = addUser({ id: socket.id, name, room });
+    if (error) return callback(error);
+    socket.join(user.room);
+    socket.emit("message", {
+      user: "admin",
+      text: `${user.name},welcome to the room ${user.room}`,
+    });
+    socket.broadcast
+      .to(user.room)
+      .emit("message", { user: "admin", text: `${user.name} has joined` });
+    io.to(user.room).emit('roomData',{room:user.room,users:getUsersInRoom(user.room)});
+    callback();
+  });
+  // callback is used to do something on frontend
+  socket.on("sendMessage", (message, callback) => {
+    const user = getUser(socket.id);
+    io.to(user.room).emit("message", { user: user.name, text: message });
+    io.to(user.room).emit('roomData',{room:user.room,users:getUsersInRoom(user.room)});
+    callback();
+  });
+  socket.on("disconnect", () => {
+    const user=removeUser(socket.id);
+    if(user){
+      io.to(user.room).emit('message',{ user:'admin',text:`${user.name} has left` });
+    }
+    console.log("user disconnected");
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`server is running on ${PORT}`);
+});
