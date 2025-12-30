ae5a606b45cf9eac4a584d0fade356b71d7955ec
sanjaysiddhu <sanjaysiddhu999@gmail.com>
2025-12-30 15:15:27 +0530

    fixed bugs

--- db/connection.js
+++ db/connection.js
@@ -1,7 +1,7 @@
 const mongoose=require('mongoose');
 
-const MONGO_URI='mongodb+srv://sanjaysiddhu999_db_user:ZsbSRthg9Qz4IC8l@cluster0.ix40ypj.mongodb.net/chatapp?appName=Cluster0'
-// const MONGO_URI=process.env.MONGO_URI;
+// const MONGO_URI='mongodb+srv://sanjaysiddhu999_db_user:ZsbSRthg9Qz4IC8l@cluster0.ix40ypj.mongodb.net/chatapp?appName=Cluster0'
+const MONGO_URI=process.env.MONGO_URI;
 
 const connectDB=async()=>{
     try{
